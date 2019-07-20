c1750e61bd4c93e47d905de22e0bfeca67135ff7
Falk Hoppe <falkhoppe81@gmail.com>
2019-07-20 07:33:39 +0200

    add summary/description and pathName to path representation

--- client/js/oc-paths.mjs
+++ client/js/oc-paths.mjs
@@ -1,10 +1,34 @@
 import { fromString, injectElements } from '/assets/client/js/oc-minitemp.mjs'
 import { renderPathOperations } from '/assets/client/js/oc-path-operations.mjs'
 
+const appendAdditionalInformation = (
+  infoType,
+  pathItem,
+  renderer,
+  options = {}
+) => {
+  const info = pathItem[infoType]
+
+  if (info) {
+    const rendered = fromString(
+      `<p class="additional-information ${infoType}">${info}</p>`
+    )
+
+    if (options.lead) {
+      rendered.classList.add('lead')
+    }
+
+    renderer.appendChild(rendered)
+  }
+}
+
 export const renderPathItem = (pathItemName, pathItem) => {
   const result = fromString(`<article>
-    <h3>${pathItem['x-link-rel']}</h3>
+    <h4>${pathItem['x-link-rel']} <strong>${pathItemName}</strong></h4>
   </article>`)
+
+  appendAdditionalInformation('summary', pathItem, result, { lead: true })
+  appendAdditionalInformation('description', pathItem, result)
 
   const operations = renderPathOperations(pathItem)
   result.appendChild(operations)
