a28e3f0ca28cfb16a3b6f076a48c587554a60ced
Falk Hoppe <falkhoppe81@gmail.com>
2019-07-26 08:03:15 +0200

    lit-html base setup

--- client/js/oc-spec.mjs
+++ client/js/oc-spec.mjs
@@ -1,4 +1,6 @@
-import { fromString } from '/assets/client/js/oc-minitemp.mjs'
+import { render } from '/assets/vendor/lit-html/lit-html.js'
+import { debugTemplate, specContainer } from './templates/oc-spec-templates.mjs'
+
 import { renderHeader } from '/assets/client/js/oc-header.mjs'
 import { renderPaths } from '/assets/client/js/oc-paths.mjs'
 import { baseUrl } from '/assets/client/js/oc-url-helper.mjs'
@@ -16,10 +18,6 @@ const loadSpec = async specPath =>
       data: res,
       baseUrl: baseUrl(specPath)
     }))
-
-const renderDebug = spec => {
-  return fromString(`<pre>${JSON.stringify(spec, false, 2)}</pre>`)
-}
 
 class OpenAPICandySpec extends HTMLElement {
   async connectedCallback () {
@@ -50,33 +48,29 @@ class OpenAPICandySpec extends HTMLElement {
   async render (spec) {
     const meta = { baseUrl: spec.baseUrl }
 
-    const container = fromString(`<div class="row">
-			<div class="oc-spec-menu col-sm-3 col-md-2"></div>
-			<div class="oc-spec-content col-sm-9 col-md-10"></div>
-		</div>`)
+    // const menu = container.querySelector('.oc-spec-menu')
+    // const content = container.querySelector('.oc-spec-content')
+    // const menuItemList = fromString('<ul class="nav flex-column"></ul>')
+    //
+    // const paths = spec.data.paths
+    //
+    // if (paths) {
+    //   Object.keys(paths).forEach(pathName => {
+    //     const path = paths[pathName]
+    //     const menuItem = fromString(`<li>
+    // 				<a href="#${path['x-link-rel']}">${path['x-link-rel']}</a>
+    // 			</li>`)
+    //     menuItemList.appendChild(menuItem)
+    //   })
+    //   menu.appendChild(menuItemList)
+    // }
 
-    const menu = container.querySelector('.oc-spec-menu')
-    const content = container.querySelector('.oc-spec-content')
-    const menuItemList = fromString('<ul class="nav flex-column"></ul>')
+    // content.append(renderHeader(spec.data.info))
+    // content.append(await renderPaths(spec.data.paths, meta))
 
-    const paths = spec.data.paths
+    // render(debugTemplate(spec), this)
 
-    if (paths) {
-      Object.keys(paths).forEach(pathName => {
-        const path = paths[pathName]
-        const menuItem = fromString(`<li>
-						<a href="#${path['x-link-rel']}">${path['x-link-rel']}</a>
-					</li>`)
-        menuItemList.appendChild(menuItem)
-      })
-      menu.appendChild(menuItemList)
-    }
-
-    content.append(renderHeader(spec.data.info))
-    content.append(await renderPaths(spec.data.paths, meta))
-    // content.append(renderDebug(spec))
-
-    this.appendChild(container)
+    // this.appendChild(container)
   }
 }
 
